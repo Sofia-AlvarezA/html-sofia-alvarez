58ec52dbcb1f5c5e196341734d82364b538da3db
Sofia-AlvarezA <123768260+Sofia-AlvarezA@users.noreply.github.com>
2023-02-13 12:38:09 -0500

    avance 3

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 document.addEventListener("DOMContentLoaded" , () => {
+const cardAdj = [
+    { name: "cab.jpg", img: "images/cab.jpg" },
+    { name: "can.jpg", img: "images/can.jpg" },
+    { name: "del.jpg", img: "images/del.jpg" },
+    { name: "eso.jpg", img: "images/eso.jpg" },
+    { name: "est.jpg", img: "images/est.jpg" },
+    { name: "tib.jpg", img: "images/tib.jpg" }
+  ];
+  
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasescogidas = [];
+  var cartasescogidasid = [];
+  var catasGanadas = [];
+  
